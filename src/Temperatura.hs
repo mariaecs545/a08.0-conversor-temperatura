6547551a5f58a0b8ae5bb1be74ec53f5c56aa574
mariaecs545 <madudasilva050405@gmail.com>
2026-05-13 11:34:13 -0300

    atv implementada em sala

--- src/Temperatura.hs
+++ src/Temperatura.hs
@@ -6,9 +6,30 @@ import Text.Printf (printf)
 import Text.Read (readMaybe)
 
 converteTemperatura :: Double -> Maybe (Double, Double)
-converteTemperatura celsius =
-  undefined
-
+converteTemperatura celsius 
+  | celsius < zeroabsoluto = Nothing
+  | otherwise              = Just (fahrenheit, kelvin)
+  where
+    fahrenheit   = celsius * 9 / 5 + 32
+    kelvin       = celsius - zeroabsoluto
+    zeroabsoluto = -273.15
+  
 main :: IO ()
-main =
-  undefined
+main = do
+  hSetBuffering stdout NoBuffering
+  putStr "Digite a temperatura em Celsius: "
+  entrada <- getLine
+  let maybeCelsius = readMaybe entrada :: Maybe Double
+  if isNothing maybeCelsius 
+    then 
+      putStrLn "Entrada inválida!"
+    else do
+      let celsius = fromJust maybeCelsius
+      let maybeTemps = converteTemperatura celsius
+      if isNothing maybeTemps
+        then
+          putStrLn "Entrada inválida!"
+        else do
+          let temps = fromJust maybeTemps
+          putStrLn (printf "Temperatura em Fahrenheit: %.2f" (fst temps))
+          putStrLn (printf "Temperatura em Kelvin: %.2f" (snd temps))
